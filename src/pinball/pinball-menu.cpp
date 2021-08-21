d36aab439940a436bc2b12ff71776a306beb386b
Phil Lutas <phil@lutas.co.uk>
2021-08-21 16:24:27 +0100

    Relative reference to image files

--- src/pinball/pinball-menu.cpp
+++ src/pinball/pinball-menu.cpp
@@ -8,54 +8,54 @@ namespace {
 
 bool PinballMenu::load()
 {
-	_plaqueGraphic.read("C:\\dev\\pinball\\pinball-score\\img\\plaques-indexed.png");
+	_plaqueGraphic.read("img/plaques-indexed.png");
 	
 	
 	_tables[0].fullname = "Ignition";
 	_tables[0].shortName = "IGN";
-	_tables[0].tableGraphic.read("C:\\dev\\pinball\\pinball-score\\img\\plaques-indexed.png");
+	_tables[0].tableGraphic.read("img/plaques-indexed.png");
 	_tables[0].plaqueGraphicOffset = 0;
 
 	_tables[1].fullname = "Steel Wheel";
 	_tables[1].shortName = "STW";
 	_tables[1].tableGraphic.read(
-	        "C:\\dev\\pinball\\pinball-score\\img\\plaques-indexed.png");
+	        "img/plaques-indexed.png");
 	_tables[1].plaqueGraphicOffset = 24;
 
 	_tables[2].fullname = "Beat Table";
 	_tables[2].shortName = "BBX";
 	_tables[2].tableGraphic.read(
-	        "C:\\dev\\pinball\\pinball-score\\img\\plaques-indexed.png");
+	        "img/plaques-indexed.png");
 	_tables[2].plaqueGraphicOffset = 48;
 
 	_tables[3].fullname = "Nightmare";
 	_tables[3].shortName = "NTM";
 	_tables[3].tableGraphic.read(
-	        "C:\\dev\\pinball\\pinball-score\\img\\plaques-indexed.png");
+	        "img/plaques-indexed.png");
 	_tables[3].plaqueGraphicOffset = 72;
 
 	_tables[4].fullname = "Neptune";
 	_tables[4].shortName = "NPT";
 	_tables[4].tableGraphic.read(
-	        "C:\\dev\\pinball\\pinball-score\\img\\plaques-indexed.png");
+	        "img/plaques-indexed.png");
 	_tables[4].plaqueGraphicOffset = 96;
 
 	_tables[5].fullname = "Safari";
 	_tables[5].shortName = "SFR";
 	_tables[5].tableGraphic.read(
-	        "C:\\dev\\pinball\\pinball-score\\img\\plaques-indexed.png");
+	        "img/plaques-indexed.png");
 	_tables[5].plaqueGraphicOffset = 120;
 
 	_tables[6].fullname = "Revenge";
 	_tables[6].shortName = "RRW";
 	_tables[6].tableGraphic.read(
-	        "C:\\dev\\pinball\\pinball-score\\img\\plaques-indexed.png");
+	        "img/plaques-indexed.png");
 	_tables[6].plaqueGraphicOffset = 144;
 
 	_tables[7].fullname = "Stall Turn";
 	_tables[7].shortName = "STT";
 	_tables[7].tableGraphic.read(
-	        "C:\\dev\\pinball\\pinball-score\\img\\plaques-indexed.png");
+	        "img/plaques-indexed.png");
 	_tables[7].plaqueGraphicOffset = 168;
 
 	return _plaqueGraphic.get_width();
